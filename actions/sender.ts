82621910dcbe5bd7de6bc991ae967255818d2ed2
Matvitsevsky <ilya.matvitsevsky@vsgcommerce.com>
2024-01-25 16:35:42 +0300

    asd

--- actions/sender.ts
+++ actions/sender.ts
@@ -10,7 +10,7 @@ interface State {
 	success: boolean
 }
 
-const TELEGRAM_BOT_TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_TOKEN
+const TELEGRAM_BOT_TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_TOKEN || ''
 const TELEGRAM_CHAT_ID =
 	process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID || '@dentPlusMessage'
 
