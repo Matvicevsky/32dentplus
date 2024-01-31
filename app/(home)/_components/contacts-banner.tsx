dd624bcd26d3da412af09f1f89e0d523004d602b
Matvitsevsky <ilya.matvitsevsky@vsgcommerce.com>
2024-01-31 21:21:27 +0300

    add skeleton

--- app/(home)/_components/contacts-banner.tsx
+++ app/(home)/_components/contacts-banner.tsx
@@ -3,8 +3,9 @@
 import dynamic from 'next/dynamic'
 import { SectionWrapper } from '@/components/section-wrapper'
 import { ContactsBannerItem } from './contacts-banner-item'
+import { YMaps } from '@pbe/react-yandex-maps'
 
-const DynamicMap = dynamic(() => import('./map'), { ssr: false })
+const DynamicMap = dynamic(async () => import('./map'), { ssr: false })
 
 export const ContactsBanner = () => {
 	return (
@@ -12,7 +13,16 @@ export const ContactsBanner = () => {
 			<div className='w-[97%] md:w-[95%] mx-auto flex flex-col gap-16 md:gap-[8.2vw]'>
 				<div className='flex w-full flex-wrap gap-4'>
 					<div className='w-full md:w-1/2 h-96'>
-						<DynamicMap />
+						<YMaps
+							query={{
+								ns: 'ymaps',
+								lang: 'ru_RU',
+								apikey: process.env.NEXT_PUBLIC_YANDEX_KEY,
+								mode: 'release',
+							}}
+						>
+							<DynamicMap />
+						</YMaps>
 					</div>
 					<div className='flex flex-col px-4 md:px-8 md:ml-[4vw]'>
 						<ContactsBannerItem />
--- app/(home)/_components/map.tsx
+++ app/(home)/_components/map.tsx
@@ -7,45 +7,56 @@ import {
 	ZoomControl,
 	GeolocationControl,
 	FullscreenControl,
+	useYMaps,
 } from '@pbe/react-yandex-maps'
 import { useAddressStore } from '@/store/use-address-store'
 import { useMemo } from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const YMap = () => {
 	const { selectedAddress } = useAddressStore()
+	const ymaps = useYMaps()
 
 	const center = useMemo(
 		() => [selectedAddress.coordinates.lat, selectedAddress.coordinates.lng],
 		[selectedAddress]
 	)
 
+	if (!ymaps) {
+		return <YMap.Skeleton />
+	}
+
 	return (
-		<YMaps
-			query={{ apikey: process.env.NEXT_PUBLIC_YANDEX_KEY, mode: 'release' }}
+		<Map
+			width='100%'
+			height='384px'
+			state={{
+				center,
+				zoom: 12,
+			}}
 		>
-			<Map
-				width='100%'
-				height='400px'
-				state={{
-					center,
-					zoom: 12,
+			<Placemark
+				geometry={center}
+				options={{
+					iconOffset: [-20, -20],
+					iconLayout: 'default#image',
+					iconImageHref: 'marker.png',
+					iconImageSize: [60, 60],
 				}}
-			>
-				<Placemark
-					geometry={center}
-					options={{
-						iconOffset: [-20, -20],
-						iconLayout: 'default#image',
-						iconImageHref: 'marker.png',
-						iconImageSize: [60, 60],
-					}}
-				/>
-				<ZoomControl options={{ position: { right: 10, top: 50 } }} />
-				<GeolocationControl options={{ float: 'right' }} />
-				<FullscreenControl options={{ float: 'left' }} />
-			</Map>
-		</YMaps>
+			/>
+			<ZoomControl options={{ position: { right: 10, top: 50 } }} />
+			<GeolocationControl options={{ float: 'right' }} />
+			<FullscreenControl options={{ float: 'left' }} />
+		</Map>
 	)
 }
 
 export default YMap
+
+YMap.Skeleton = function YMapSkeleton() {
+	return (
+		<div className='relative w-full h-96'>
+			<Skeleton className='w-ful h-96' />
+		</div>
+	)
+}
