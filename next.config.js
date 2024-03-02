93f5c248c3acc8b4dc26ee27fe2187ef21b902c3
Matvitsevsky <ilya.matvitsevsky@amitechgrp.com>
2024-03-02 11:33:49 +0300

    google indexing redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 
+const { redirect } = require('next/dist/server/api-utils')
+
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
 	reactStrictMode: true,
@@ -9,6 +11,30 @@ const nextConfig = {
 		unoptimized: true,
 	},
 	basePath: '',
+	async redirect() {
+		return [
+			{
+				source: '/uslugi_cena',
+				destination: '/',
+				permanent: true,
+			},
+			{
+				source: '/tur_po_klinike',
+				destination: '/',
+				permanent: true,
+			},
+			{
+				source: '/komanda-vrachey',
+				destination: '/doctors',
+				permanent: true,
+			},
+			{
+				source: '/ortodonty',
+				destination: '/',
+				permanent: true,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
