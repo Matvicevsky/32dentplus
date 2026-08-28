124e0f87f8a6df6091c4b02831b04d6ed0abd979
IMatvicevsky <matvicevsky_ilya@mail.ru>
2026-08-28 13:17:22 +0300

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,50 @@
-import 'slick-carousel/slick/slick-theme.css'
-import 'slick-carousel/slick/slick.css'
-import 'react-photo-view/dist/react-photo-view.css'
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'react-photo-view/dist/react-photo-view.css';
 
-import { Roboto } from 'next/font/google'
+import { Roboto } from 'next/font/google';
 
-import './globals.css'
+import './globals.css';
 
-import ModalProvider from '@/components/provaiders/modal-provider'
-import StoreProvider from '@/components/provaiders/store-provider'
-import { Toaster } from '@/components/ui/toaster'
+import ModalProvider from '@/components/provaiders/modal-provider';
+import StoreProvider from '@/components/provaiders/store-provider';
+import { Toaster } from '@/components/ui/toaster';
 
-import Navbar from '@/components/navbar'
-import { Footer } from '@/components/footer'
+import Navbar from '@/components/navbar';
+import { Footer } from '@/components/footer';
 
-import { cn } from '@/lib/utils'
-import HydrationZustand from '@/components/hydration-zustand'
-import { getPageData } from '@/lib/get-page-data'
+import { cn } from '@/lib/utils';
+import HydrationZustand from '@/components/hydration-zustand';
+import { getPageData } from '@/lib/get-page-data';
+import Analytics from '@/components/analytics';
+import { Suspense } from 'react';
+import { NextFont } from 'next/dist/compiled/@next/font';
 
-const roboto = Roboto({
-	subsets: ['cyrillic', 'latin'],
-	weight: ['100', '300', '400', '500', '700', '900'],
-})
+const roboto: NextFont = Roboto({
+  subsets: ['cyrillic', 'latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+});
 
-export default async function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
-	const { navLinks } = await getPageData()
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const { navLinks } = await getPageData();
 
-	return (
-		<html className='scroll-smooth' lang='ru'>
-			<body
-				className={cn(
-					'bg-secondary text-primary relative font-light',
-					roboto.className
-				)}
-			>
-				<HydrationZustand>
-					<StoreProvider>
-						<ModalProvider />
-						<Navbar items={navLinks} />
-						{children}
-						<Toaster />
-						<Footer />
-					</StoreProvider>
-				</HydrationZustand>
-			</body>
-		</html>
-	)
+  return (
+    <html className="scroll-smooth" lang="ru">
+      <body className={cn('bg-secondary text-primary relative font-light', roboto.className)}>
+        <Suspense fallback={null}>
+          <Analytics />
+        </Suspense>
+
+        <HydrationZustand>
+          <StoreProvider>
+            <ModalProvider />
+            <Navbar items={navLinks} />
+            {children}
+            <Toaster />
+            <Footer />
+          </StoreProvider>
+        </HydrationZustand>
+      </body>
+    </html>
+  );
 }
